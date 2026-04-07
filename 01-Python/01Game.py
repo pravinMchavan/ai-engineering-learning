eda0936ab1cc06a9c7625eb001ff6c2f02f0b016
pravinMchavan <finalweb7887@gmail.com>
2026-04-07 09:52:32 +0530

    Guess the Number using Fucntion and Methods

--- 01-Python/01Game.py
+++ 01-Python/01Game.py
@@ -4,29 +4,62 @@ round = 0
 total_round = 5
 users = ["Alice", "Bob", "Charlie"]
 
+
 # Fix scoreboard keys
 score_board = {"Alice": 0, "Bob": 0, "Charlie": 0}
 
+# while round < total_round:
+#     print(f"\n--- Round {round + 1} ---")
+    
+#     random_number = random.randint(1, 9)
+#     print("Random number is:", random_number)
+
+#     for user in users:
+#         user_input = int(input(f"{user}, guess a number (1-9): "))
+
+#         if user_input < 1 or user_input > 9:
+#             print("Please enter a number between 1 and 9.")
+#         elif user_input == random_number:
+#             print("Correct guess!")
+#             score_board[user] += 1   
+#         else:
+#             print("No match!")
+
+#     round += 1   
+
+# # Final Scores
+# print("\nFinal Scores:")
+# for user, score in score_board.items():
+#     print(f"{user}: {score}")
+
+#--------------------- Using Method ---------------------
+
+def generate_random_number():
+    return random.randint(1, 9)
+
+def verifyNumber(user, user_input, random_number):
+    if user_input < 1 or user_input > 9:
+        print("Please enter a number between 1 and 9.")
+    elif user_input == random_number:
+        print("Correct guess!")
+        score_board[user] += 1
+    else:
+        print("No match!")
+
+# Game loop
 while round < total_round:
     print(f"\n--- Round {round + 1} ---")
     
-    random_number = random.randint(1, 9)
-    print("Random number is:", random_number)
+    random_number = generate_random_number()
+    print("Random Number:", random_number)
 
     for user in users:
         user_input = int(input(f"{user}, guess a number (1-9): "))
+        verifyNumber(user, user_input, random_number)
 
-        if user_input < 1 or user_input > 9:
-            print("Please enter a number between 1 and 9.")
-        elif user_input == random_number:
-            print("Correct guess!")
-            score_board[user] += 1   
-        else:
-            print("No match!")
+    round += 1
 
-    round += 1   
-
-# Final Scores
+# Final scores
 print("\nFinal Scores:")
 for user, score in score_board.items():
     print(f"{user}: {score}")
