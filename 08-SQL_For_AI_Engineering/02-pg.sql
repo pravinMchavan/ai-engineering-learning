164a20dbc1a8d45675271bbeb7b143e217fc6463
pravinMchavan <finalweb7887@gmail.com>
2026-04-24 17:51:43 +0530

    Changed file name

--- 08-SQL_For_AI_Engineering/02-pg.sql
+++ 08-SQL_For_AI_Engineering/02-pg.sql
@@ -1,11 +1,23 @@
-CREATE tABLE products(
-name VARCHAR(100),
-price INT,
-description VARCHAR(200)
+#--SQL code to create a table named 'cities' with columns for name, country, population, and area.
+CREATE TABLE cities( 
+name VARCHAR(50),
+country VARCHAR(50),
+population INTEGER,
+area INTEGER
 );
-#--Inserting data into the 'products' table for a product named 'Laptop'.
-INSERT INTO products VALUES ('Laptop', 1000, 'A high-performance laptop suitable for gaming');
-#--Adding a new column 'brand' to the 'products' table.
-ALTER TABLE products ADD Column brand VARCHAR(50);
+#--Inserting data into the 'cities' table for Delhi and Kochi.
+INSERT INTO cities VALUES ('Delhi', 'India', 10, 10000);
+INSERT INTO cities VALUES ('Delhi', 'India', 10, 10000), ('Kochi', 'India', 4, 10000);
+#--Query to select all records from the 'cities' table.
+SELECT * from cities;
+#--Query to select records from the 'cities' table where the name is 'Kochi'.
+SELECT * from cities WHERE name = 'Kochi';
 
-SELECT * from products;
+#--Query to update the population of Kochi to 5 in the 'cities' table.
+UPDATE cities SET population = 5 WHERE name='Kochi'
+#--Query to delete the record of Kochi from the 'cities' table.
+DELETE FROM cities WHERE name='Kochi';
+#--Query to select all records from the 'cities' table after deletion.
+DROP TABLE cities;
+#--Query to delete all records from the 'cities' table without deleting the table itself.
+TRUNCATE TABLE cities;
